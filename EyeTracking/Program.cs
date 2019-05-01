df7941d05f48262f76d466f6b4b1005ae2e0125e
Adil Ghaffar <adilghaffar64@gmail.com>
2019-05-01 22:33:28 +0300

    sample data printed in console

--- EyeTracking/Program.cs
+++ EyeTracking/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace EyeTracking
@@ -8,6 +9,8 @@ namespace EyeTracking
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Generator SampleDataGenerator = new Generator(3,100,250,300,500,50,1024,768,60,53.34f);
+            SampleDataGenerator.GenerateSampleData();
         }
 
     }
@@ -24,6 +27,10 @@ namespace EyeTracking
             this.yCord = yCord;
             this.timeStamp = timeStamp;
         }
+
+        public float XCord { get => xCord; set => xCord = value; }
+        public float YCord { get => yCord; set => yCord = value; }
+        public string TimeStamp { get => timeStamp; set => timeStamp = value; }
     }
 
     enum GazeMovementState
@@ -67,19 +74,26 @@ namespace EyeTracking
 
         GazeMovementState currentState;
 
-        float ContinousFixationTime = 0;
-        //float ContinousFixationTime = 0;
+        float minDegreeForFixation;
+        float maxDegreeForFixation;
+
+        float ContinousFixationTime;
+
+        int totalDataPoints;
+
+        float minFixationPix;
+        float maxFixationPix;
+
         DataPoint[] dataPoints;
 
-        public Generator(float duration, float fixationMin, float fixationMax, float saccadeMin, float saccadeMax, float velocityMin, float velocityMax, float tempoaralRes, float resX, float resY, float distPartcipantScreen, float disaplaySize)
+        public Generator(float duration, float fixationMin, float fixationMax, float saccadeMin, float saccadeMax, float tempoaralRes, float resX, float resY, float distPartcipantScreen, float disaplaySize)
         {
             this.duration = duration;
             this.fixationMin = fixationMin;
             this.fixationMax = fixationMax;
             this.saccadeMin = saccadeMin;
             this.saccadeMax = saccadeMax;
-            this.velocityMin = velocityMin;
-            this.velocityMax = velocityMax;
+          
             this.tempoaralRes = tempoaralRes;
             this.resX = resX;
             this.resY = resY;
@@ -88,20 +102,30 @@ namespace EyeTracking
 
             degPerPixel = RadianToDegree(MathF.Atan2(0.5f * disaplaySize, distPartcipantScreen)) / (0.5f * resY);
             currentState = GazeMovementState.none;
+            ContinousFixationTime = 0;
+            minDegreeForFixation = 1;
+            maxDegreeForFixation = 5;
+
+            totalDataPoints = (int)(duration * tempoaralRes);
+
+            minFixationPix = ((1 / degPerPixel) * minDegreeForFixation) / totalDataPoints;
+            maxFixationPix = ((1 / degPerPixel) * maxDegreeForFixation) / totalDataPoints;
 
         }
+
         int currentDataIndex = 0;
+
         public string GenerateSampleData()
         {
             string fileName = "SampleData.csv";
-            int totalDataPoints = (int)(duration * tempoaralRes);
             dataPoints = new DataPoint[totalDataPoints];
             SwitchState();
-            for (int currentDataIndex = 0; currentDataIndex < totalDataPoints; currentDataIndex++)
+            for (currentDataIndex = 0; currentDataIndex < totalDataPoints; currentDataIndex++)
             {
                 if(currentState == GazeMovementState.slow)
                 {
                     GenerateFixationPoint();
+                    ContinousFixationTime += (duration*1000) / totalDataPoints;
                 }
                 else
                 {
@@ -130,12 +154,32 @@ namespace EyeTracking
             else
             {
                 DataPoint prevPoint = dataPoints[currentDataIndex-1];
+                var ranX = GetRandomNumberFloat(minFixationPix,maxFixationPix);
+                var ranY = GetRandomNumberFloat(minFixationPix, maxFixationPix);
+                dataPoints[currentDataIndex] = new DataPoint(prevPoint.XCord + (float)ranX, prevPoint.XCord + (float)ranY, System.DateTime.Now.ToLongTimeString());
+                Console.Write("xcord: "+ dataPoints[currentDataIndex].XCord+"  "+ "ycord: " + dataPoints[currentDataIndex].YCord+"  "+ "Time: "+ dataPoints[currentDataIndex].TimeStamp + "\n");
             }
         }
 
         private void GenerateSaccadePoint()
         {
+            if (currentDataIndex == 0)
+            {
+                dataPoints[currentDataIndex] = new DataPoint(-1, -1, System.DateTime.Now.ToLongTimeString());
+                Console.Write("xcord: " + dataPoints[currentDataIndex].XCord + "  " + "ycord: " + dataPoints[currentDataIndex].YCord + "  " + "Time: " + dataPoints[currentDataIndex].TimeStamp + "\n");
 
+            }
+            else
+            {
+                dataPoints[currentDataIndex] = new DataPoint(-1, -1, System.DateTime.Now.ToLongTimeString());
+                Console.Write("xcord: " + dataPoints[currentDataIndex].XCord + "  " + "ycord: " + dataPoints[currentDataIndex].YCord + "  " + "Time: " + dataPoints[currentDataIndex].TimeStamp+"\n");
+
+
+                //DataPoint prevPoint = dataPoints[currentDataIndex - 1];
+                //var ranX = GetRandomNumberFloat(minFixationPix, maxFixationPix);
+                //var ranY = GetRandomNumberFloat(minFixationPix, maxFixationPix);
+                //dataPoints[currentDataIndex] = new DataPoint(prevPoint.XCord + (float)ranX, prevPoint.XCord + (float)ranY, System.DateTime.Now.ToLongTimeString());
+            }
         }
 
         private void SwitchState()
@@ -148,11 +192,10 @@ namespace EyeTracking
             else if(ContinousFixationTime < fixationMin)
             {
                 currentState = GazeMovementState.slow;
-                ContinousFixationTime += 0;
             }
             else
             {
-                int r = GetRandomNumber(0,100);
+                float r = GetRandomNumber(0,100);
                 if(r < 50)
                 {
                     currentState = GazeMovementState.fast;
@@ -167,8 +210,13 @@ namespace EyeTracking
         {
             return getrandom.Next(min, max);
         }
+
+        public double GetRandomNumberFloat(double minimum, double maximum)
+        {
+            return getrandom.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+
     }
-
-
 
 }
